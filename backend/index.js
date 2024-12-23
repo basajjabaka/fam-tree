c0ec31d483eb7e86883a4b96894490b15d7ab621
ethoxi <staneswilson2006@gmail.com>
2024-12-23 17:50:56 +0530

    Render.com config

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ const fs = require("fs");
 const moment = require("moment");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
